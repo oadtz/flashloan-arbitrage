cdd0cac0fd6cb9d51e7a07c5eba9117cb2a32525
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-03-31 22:23:10 +0700

    no message

--- src/utils/arbitrage.ts
+++ src/utils/arbitrage.ts
@@ -62,7 +62,11 @@ export async function run(
     )
       continue;
 
-    const amountIn = toDecimals(borrowedAmount, token0.decimals);
+    const randomFactor = 0.5 + Math.random() * 0.5; // Generate a random factor between 0.5 and 1
+    const amountIn =
+      (toDecimals(borrowedAmount, token0.decimals) *
+        BigInt(Math.floor(randomFactor * 100))) /
+      BigInt(100);
     const expactedAmountOut =
       (amountIn * BigInt((1 + flashLoanFee) * 100_000)) / BigInt(100_000);
 
