e677d0317734d3261dab324e8fd4fe682422ee38
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-04-21 10:46:18 +0700

    no message

--- src/config/app.ts
+++ src/config/app.ts
@@ -8,5 +8,5 @@ export const appConfig = {
   sepoliaRpcUrl: "https://ethereum-sepolia-rpc.publicnode.com",
   ethereumRpcUrl: "https://eth-pokt.nodies.app",
   bscRpcUrl: `https://bsc-dataseed3.binance.org/`,
-  polygonRpcUrl: `https://polygon-bor-rpc.publicnode.com`,
+  polygonRpcUrl: `https://polygon-rpc.com`,
 };
