71fb70d749b1747d8a16d774d5e649987b5af440
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-04-25 19:39:42 +0700

    no message

--- src/utils/trade-analysis.ts
+++ src/utils/trade-analysis.ts
@@ -4,6 +4,7 @@ import {
   EMA,
   SMA,
   StochasticRSI,
+  Stochastic,
   ADX,
   BollingerBands,
 } from "technicalindicators";
@@ -14,16 +15,16 @@ export function isSellSignal(data: number[]): {
 } {
   let price = [...data];
 
-  if (data.length < 21) {
-    const chunkSize = Math.round(21 / data.length);
+  if (data.length < 29) {
+    const chunkSize = Math.round(29 / data.length);
 
-    for (let i = 0; i < 21; i++) {
+    for (let i = 0; i < 29; i++) {
       const chunkIndex = Math.floor(i / chunkSize);
       price[i] = data[chunkIndex] ? data[chunkIndex] : data[data.length - 1];
     }
   }
 
-  const rsi = RSI.calculate({ values: price, period: 14 });
+  // const rsi = RSI.calculate({ values: price, period: 14 });
   const macd = MACD.calculate({
     values: price,
     fastPeriod: 9,
@@ -56,15 +57,15 @@ export function isSellSignal(data: number[]): {
   //   SimpleMAOscillator: false,
   //   SimpleMASignal: false,
   // });
-  const ema = EMA.calculate({ period: 50, values: price });
-  const sma = SMA.calculate({ period: 200, values: price });
-  const stochRsi = StochasticRSI.calculate({
-    values: price,
-    rsiPeriod: 14,
-    stochasticPeriod: 14,
-    kPeriod: 3,
-    dPeriod: 3,
-  });
+  // const ema = EMA.calculate({ period: 50, values: price });
+  // const sma = SMA.calculate({ period: 200, values: price });
+  // const stochRsi = StochasticRSI.calculate({
+  //   values: price,
+  //   rsiPeriod: 14,
+  //   stochasticPeriod: 14,
+  //   kPeriod: 3,
+  //   dPeriod: 3,
+  // });
   // const stochRsi = StochasticRSI.calculate({
   //   values: price,
   //   rsiPeriod: /*price.length < 323 ? 14 :*/ 75,
@@ -72,35 +73,49 @@ export function isSellSignal(data: number[]): {
   //   kPeriod: /*price.length < 323 ? 3 :*/ 50,
   //   dPeriod: /*price.length < 323 ? 3 :*/ 50,
   // });
-  const adx = ADX.calculate({
-    period: 14,
+  const stochastic = Stochastic.calculate({
     high: price,
     low: price,
     close: price,
+    period: 14,
+    signalPeriod: 3,
   });
-  const bollinger = BollingerBands.calculate({
-    period: 20,
-    values: price,
-    stdDev: 2,
-  });
+  // const adx = ADX.calculate({
+  //   period: 14,
+  //   high: price,
+  //   low: price,
+  //   close: price,
+  // });
+  // const bollinger = BollingerBands.calculate({
+  //   period: 20,
+  //   values: price,
+  //   stdDev: 2,
+  // });
 
   const currentPrice = price[price.length - 1];
-  const rsiValue = rsi[rsi.length - 1];
+  // const rsiValue = rsi[rsi.length - 1];
   const macdValue = macd[macd.length - 1]?.MACD || 0;
   const macdSignalValue = macd[macd.length - 1]?.signal || 0;
-  const emaValue = ema[ema.length - 1];
-  const smaValue = sma[sma.length - 1];
-  const stochRsiValueK = stochRsi[stochRsi.length - 1]?.k || 0;
-  const stochRsiValueD = stochRsi[stochRsi.length - 1]?.d || 0;
-  const adxValue = adx[adx.length - 1]?.adx;
-  const adxPreviousValue = adx[adx.length - 2]?.adx;
-  const bollingerUpper = bollinger[bollinger.length - 1]?.upper;
-  const bollingerMiddle = bollinger[bollinger.length - 1]?.middle;
+  const stochasticValueK = stochastic[stochastic.length - 1]?.k || 0;
+  const stochasticValueD = stochastic[stochastic.length - 1]?.d || 0;
+  // const emaValue = ema[ema.length - 1];
+  // const smaValue = sma[sma.length - 1];
+  // const stochRsiValueK = stochRsi[stochRsi.length - 1]?.k || 0;
+  // const stochRsiValueD = stochRsi[stochRsi.length - 1]?.d || 0;
+  // const adxValue = adx[adx.length - 1]?.adx;
+  // const adxPreviousValue = adx[adx.length - 2]?.adx;
+  // const bollingerUpper = bollinger[bollinger.length - 1]?.upper;
+  // const bollingerMiddle = bollinger[bollinger.length - 1]?.middle;
+  // const bollingerLower = bollinger[bollinger.length - 1]?.lower;
 
   const sellSignal =
     macdValue < macdSignalValue &&
     macdSignalValue !== 0 &&
-    currentPrice < bollingerMiddle &&
+    stochasticValueK < stochasticValueD &&
+    //stochasticValueD < 80 &&
+    // stochasticValueK < 80 &&
+    // currentPrice < bollingerMiddle &&
+    //adxValue > 25 &&
     //currentPrice < emaValue &&
     //currentPrice < smaValue &&
     //emaValue < smaValue &&
@@ -111,21 +126,22 @@ export function isSellSignal(data: number[]): {
     // stochRsiValueK < stochRsiValueD &&
     // stochRsiValueD < 20 &&
     // stochRsiValueK < 20 &&
-    //adxValue < adxPreviousValue &&
     //currentPrice > bollingerUpper &&
     true;
 
   return {
     sell: sellSignal,
     indicators: {
-      ema: emaValue,
-      sma: smaValue,
+      // ema: emaValue,
+      // sma: smaValue,
       macd: macdValue,
       macdSignal: macdSignalValue,
-      stochRsiK: stochRsiValueK,
-      stochRsiD: stochRsiValueD,
-      adx: adxValue,
-      adxPrevious: adxPreviousValue
+      stochasticK: stochasticValueK,
+      stochasticD: stochasticValueD,
+      // stochRsiK: stochRsiValueK,
+      // stochRsiD: stochRsiValueD,
+      // adx: adxValue,
+      // adxPrevious: adxPreviousValue,
     },
   };
 }
--- src/utils/trade.ts
+++ src/utils/trade.ts
@@ -209,10 +209,10 @@ export async function run(
           +formatDecimals(amountToken, tokenToTrade.decimals)
         } ETH/${getAssetName(tokenToTrade.address)}`
       );
-      console.log(`MACD: ${indicators.macd}`);
       console.log(`MACD Signal: ${indicators.macdSignal}`);
-      console.log(`Stoch RSI D: ${indicators.stochRsiD}`);
-      console.log(`Stoch RSI K: ${indicators.stochRsiK}`);
+      console.log(`MACD: ${indicators.macd}`);
+      console.log(`Stochastic D: ${indicators.stochasticD}`);
+      console.log(`Stochastic K: ${indicators.stochasticK}`);
       console.log(`Sell Signal: ${sellSignal}`);
 
       if (
