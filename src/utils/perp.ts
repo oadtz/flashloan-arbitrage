c8f23e61371fcb5ef3129c354dbdebdeb4a29bdb
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-05-01 00:35:12 +0700

    no message

--- src/utils/perp.ts
+++ src/utils/perp.ts
@@ -108,6 +108,7 @@ export async function run(
           openPosition.pnl = BigInt(0);
 
           currentBalance = await getBalance(provider);
+          await new Promise((resolve) => setTimeout(resolve, delay));
         }
       }
 
