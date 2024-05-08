1bd05a9caed1cdf2d9f0e5e5a55ada8e162f72f7
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-05-08 19:08:29 +0700

    no message

--- src/utils/perp.ts
+++ src/utils/perp.ts
@@ -147,7 +147,7 @@ export async function run(
 
         const result = await openTrade(
           tokenToTrade.address,
-          false,
+          true,
           currentBalance / BigInt(2),
           currentPrice,
           perpContractAddress,
@@ -158,10 +158,10 @@ export async function run(
           console.log(`Opened short trade#${++shortCount}`);
 
           openPosition.amount = currentBalance / BigInt(2);
-          openPosition.price = (currentPrice * BigInt(998)) / BigInt(1000);
-          _lastPosition = "short";
+          openPosition.price = (currentPrice * BigInt(1002)) / BigInt(1000);
+          _lastPosition = "long";
         } else {
-          console.log("☹️ Cannot open short trade, wait for next signal");
+          console.log("☹️ Cannot open long trade, wait for next signal");
           process.exit(1);
         }
       } else if (_lastPosition === null && longSignal) {
@@ -180,7 +180,7 @@ export async function run(
 
         const result = await openTrade(
           tokenToTrade.address,
-          true,
+          false,
           currentBalance / BigInt(2),
           currentPrice,
           perpContractAddress,
@@ -191,10 +191,10 @@ export async function run(
           console.log(`Opened long trade#${++longCount}`);
 
           openPosition.amount = currentBalance / BigInt(2);
-          openPosition.price = (currentPrice * BigInt(1002)) / BigInt(1000);
-          _lastPosition = "long";
+          openPosition.price = (currentPrice * BigInt(998)) / BigInt(1000);
+          _lastPosition = "short";
         } else {
-          console.log("☹️ Cannot open long trade, wait for next signal");
+          console.log("☹️ Cannot open short trade, wait for next signal");
           process.exit(1);
         }
       } else {
