c821bedb502a3404255442b5bc2c17fd3a4e0493
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-05-09 17:49:47 +0700

    no message

--- src/utils/trade-analysis.ts
+++ src/utils/trade-analysis.ts
@@ -102,7 +102,7 @@ export function isShortSignal(
     //longTermTrend < 0 &&
     shortTermSignal < longTermSignal &&
     lastPrice > bbandSignal &&
-    longTermSignal >= 0.1;
+    longTermSignal >= 0.15;
 
   return {
     short: shortSignal,
@@ -166,7 +166,7 @@ export function isLongSignal(
     //longTermTrend > 0 &&
     shortTermSignal > longTermSignal &&
     lastPrice < bbandSignal &&
-    longTermSignal <= -0.1;
+    longTermSignal <= -0.15;
 
   return {
     long: longSignal,
