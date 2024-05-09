6209e4e2a43d150abe2d42604222d2a0b8774d0f
Thanapat Pirmphol <t.pirmphol@gmail.com>
2024-05-09 22:00:00 +0700

    no message

--- src/utils/isOperatingHour.ts
+++ src/utils/isOperatingHour.ts
@@ -5,7 +5,7 @@ export default function isOperatingHour(): boolean {
 
     // Check if time is exactly between 6:00 AM and 4:00 PM
     const currentTimeInMinutes = hours * 60 + minutes;
-    const start = 8 * 60;   // 6:00 AM in minutes
+    const start = 6 * 60;   // 6:00 AM in minutes
     const end = 16 * 60;    // 4:00 PM in minutes
 
     return currentTimeInMinutes >= start && currentTimeInMinutes < end;
